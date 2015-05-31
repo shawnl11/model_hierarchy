aaa6f0f6f281631f28536e340821956f23d1d893
shawnl11 <shawn.m.lee11@gmail.com>
2015-05-30 18:45:24 -0700

    stub methods

--- railsbridge/testauto/app/models/accounthistory.rb
+++ railsbridge/testauto/app/models/accounthistory.rb
@@ -1,3 +1,15 @@
 class Accounthistory < ActiveRecord::Base
 	belongs_to :account
+
+	def sort_asc(column)
+		#sorts account history ascending
+	end
+
+	def sort_desc(column)
+		#sorts account history descending
+	end
+
+	def search_data(filter)
+		#returns data based on filter
+	end
 end
--- railsbridge/testauto/app/models/autotestsuite.rb
+++ railsbridge/testauto/app/models/autotestsuite.rb
@@ -2,4 +2,16 @@ class Autotestsuite < ActiveRecord::Base
 	has_many :apptestbundles
 	has_one :reportingtool
 	has_one :account
+
+	def view_reports()
+		#view reporting tools
+	end
+
+	def select_web_app()
+		#select/create web application to create test scenarios for
+	end
+
+	def view_accounts()
+		#view account options
+	end
 end
